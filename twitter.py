6f9b582e9d708ff586c3916b5b94d9d475f1bf83
vaibssingh <singhvaibhav592@gmail.com>
2018-01-11 23:02:17 +0530

    Quick commit

--- twitter.py
+++ twitter.py
@@ -2,6 +2,7 @@ import tweepy
 import json
 import re
 from tweepy import OAuthHandler
+from nltk.tokenize import word_tokenize
 
 #Using OAuth interface to authorize our app to access Twitter
 consumer_key = "qKgqkgL14FOygq41iejwwkdvi"
@@ -51,3 +52,48 @@ with open('sense.json', 'r') as f:
     line = f.readline()  # read only the first tweet/line
     tweet = json.loads(line)  # load it as Python dict
     print(json.dumps(tweet, indent=4))  # pretty-print
+
+#Writing regex rules to consider various aspects of a tweet
+
+emoticons_str = r"""
+    (?:
+        [:=;] # Eyes
+        [oO\-]? # Nose (optional)
+        [D\)\]\(\]/\\OpP] # Mouth
+    )"""
+
+regex_str = [
+    emoticons_str,
+    r'<[^>]+>',  # HTML tags
+    r'(?:@[\w_]+)',  # @-mentions
+    r"(?:\#+[\w_]+[\w\'_\-]*[\w_]+)",  # hash-tags
+    # URLs
+    r'http[s]?://(?:[a-z]|[0-9]|[$-_@.&amp;+]|[!*\(\),]|(?:%[0-9a-f][0-9a-f]))+',
+
+    r'(?:(?:\d+,?)+(?:\.?\d+)?)',  # numbers
+    r"(?:[a-z][a-z'\-_]+[a-z])",  # words with - and '
+    r'(?:[\w_]+)',  # other words
+    r'(?:\S)'  # anything else
+]
+
+tokens_re = re.compile(r'(' + '|'.join(regex_str) + ')',re.VERBOSE | re.IGNORECASE)
+emoticon_re = re.compile(r'^' + emoticons_str + '$',re.VERBOSE | re.IGNORECASE)
+
+def tokenize(s):
+    return tokens_re.findall(s)
+
+def preprocess(s, lowercase=False):
+    tokens = tokenize(s)
+    if lowercase:
+        tokens = [token if emoticon_re.search(
+            token) else token.lower() for token in tokens]
+    return tokens
+
+#To process all our tweets, previously saved on file
+
+
+with open('sense.json', 'r') as f:
+    for line in f:
+        tweet = json.loads(line)
+        tokens = preprocess(tweet['text'])
+        do_something_else(tokens)
