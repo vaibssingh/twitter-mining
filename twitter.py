0da5e24ec5ac49b0997698054d8dd1a172a03abd
Vaibhav <singhvaibhav592@gmail.com>
2018-01-17 15:56:25 +0530

    Update twitter.py
    Almost there

--- twitter.py
+++ twitter.py
@@ -10,11 +10,10 @@ from nltk import bigrams
 from collections import Counter
 
 #Using OAuth interface to authorize our app to access Twitter
-consumer_key = "qKgqkgL14FOygq41iejwwkdvi"
-consumer_secret = "PgbqHNTMQvBB6pVvsDRFJqxJqv3jcR49JygMUpUaRQB8cUJ0la"
-access_token = "166564853-F9sSz9LI3TddGslool07Hsjah0Kw6rTT4fdxuJn7"
-access_secret = "oMC6oHIbiMLk0G9Lg499F1Q2qe7dSs1PniYjRyMTs6lI0"
-
+consumer_key = "YOUR_CONSUMER_KEY"
+consumer_secret = "YOUR_CONSUMER_SECRET"
+access_token = "YOUR_ACCESS_TOKEN"
+access_secret = "YOUR_ACCESS_SECRET"
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
